dff4a6b7c1134130e47dfcd82875e50fc74ae3b6
unknown <Administrateur@LOCAL.FRANKFURT.CUB.SIOPLC.FR>
2024-03-13 10:36:48 +0100

    Exercice

---
+++ Exercice1-3.ps1
@@ -0,0 +1,7 @@
+﻿#Exercice1-3
+#Frahati AHAMADI
+
+# Afficher les processus de "svchost" en cours d'execution
+
+$Processus = Get-Process | Where-Object {$_.ProcessName -eq "svchost"} 
+$Processus | Format-Table Id, Name, Description
