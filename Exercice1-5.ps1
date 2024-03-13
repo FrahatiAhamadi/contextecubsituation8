84229349b62e266315cd73c1bea45916d63acf47
unknown <Administrateur@LOCAL.FRANKFURT.CUB.SIOPLC.FR>
2024-03-13 11:08:18 +0100

    ScriptInfoprocessus

---
+++ Exercice1-5.ps1
@@ -0,0 +1,14 @@
+﻿##Exercice1-5
+#Frahati AHAMADI
+#13/03/2024
+
+#Ecrire une fonction permettant d'afficher le processus désirer par un utilisateur
+
+function InfoProcessus($processusName)
+{
+$Processus = Get-Process | Where-Object {$_.ProcessName -eq "$ProcessusName"} 
+$Processus | Format-Table Id, Name, Description
+}
+$ProcessusName = Read-Host "quel processus désirez vous afficher"
+
+InfoProcessus($ProcessusName)
