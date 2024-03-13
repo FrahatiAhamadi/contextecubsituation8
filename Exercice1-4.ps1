e2c368b0d1b808e9b553b290b070dcec487d6183
unknown <Administrateur@LOCAL.FRANKFURT.CUB.SIOPLC.FR>
2024-03-13 10:51:23 +0100

    Exercice1,4

---
+++ Exercice1-4.ps1
@@ -0,0 +1,7 @@
+﻿#Exercice1-4
+#Frahati AHAMADI
+#13/03/2024
+$ProcessusName = Read-Host "quel processus désirez vous afficher"
+
+$Processus = Get-Process | Where-Object {$_.ProcessName -like "$ProcessusName"} 
+$Processus | Format-Table Id, Name, Description
