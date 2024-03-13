a529ad81a8b3e2991a00e5bf3702b9dfd3bcabdc
unknown <Administrateur@local.frankfurt.cub.sioplc.fr>
2024-03-13 10:03:34 +0100

    Exercice1-2

---
+++ Exercice1-2.ps1
@@ -0,0 +1,9 @@
+﻿#Scripting powershell.ps1
+#FRAHATI AHAMADI
+#Exercice 1-2
+#13/03/2024
+
+#Commande listant l'ensemble des processus en cours sur le serveur
+
+#On va lister les numéros d'identifiants et les noms les processus
+Get-Process | Where-Object {$_.ProcessName -eq "svchost"} | Format-Table -AutoSize
