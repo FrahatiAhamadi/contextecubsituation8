43e5c857b2030b1b66c0d8828c30077f784d55ba
unknown <Administrateur@LOCAL.FRANKFURT.CUB.SIOPLC.FR>
2024-03-20 10:58:15 +0100

    Scriptservice

---
+++ Exercice1-7.ps1
@@ -0,0 +1,15 @@
+﻿ ##frahati AHAMADI
+ ##20/03/2024
+ 
+ 
+ # Function to get and export information about running services
+function Export-RunningServices {
+    # Get running services
+    $RunningServices = Get-Service | Where-Object { $_.Status -eq 'Running' }
+
+    # Export running services to CSV file
+    $RunningServices | Export-Csv -Path "C:\git_cub\contextecubsituation8\services.csv" -NoTypeInformation
+}
+
+# Call the function to export running services
+Export-RunningServices
